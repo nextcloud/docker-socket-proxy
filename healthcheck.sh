2d9a40b6290290509337befe948fa1eff0950e75
Andrey Borysenko <andrey18106x@gmail.com>
2024-01-31 13:19:47 +0200

    fix(healthcheck): adjust to use BIND_ADDRESS if it is set (#7)
    * fix(healthcheck): adjust to use BIND_ADDRESS if it is set

--- healthcheck.sh
+++ healthcheck.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-nc -z localhost "$HAPROXY_PORT" || exit 1
+if [ "$BIND_ADDRESS" != "*" ]; then
+	nc -z "$BIND_ADDRESS" "$HAPROXY_PORT" || exit 1
+else
+	nc -z localhost "$HAPROXY_PORT" || exit 1
+fi
